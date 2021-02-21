9fd8c3e31395ffeed458b67d88ea00328350fa93
eoaydin <67283599+eoaydin@users.noreply.github.com>
2021-02-22 01:16:09 +0300

    Add project files.

---
+++ ConsoleUserInterface/Program.cs
@@ -0,0 +1,45 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUserInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            Console.WriteLine("----------GetAll----------");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+            Console.WriteLine("--------GetByBrandId-------");
+            foreach (var car in carManager.GetByBrandId(2))
+            {
+                Console.WriteLine(car.Description);
+            }
+            Console.WriteLine("-----------Delete----------");
+            carManager.Delete(new Entities.Concrete.Car { Id = 3, BrandId = 2, ColorId = 5, DailyPrice = 390000, ModelYear = 2021, Description = "Audi A3" });
+            Console.WriteLine("The Car Deleted. New Car List is Below");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+            Console.WriteLine("------------Add------------");
+            carManager.Add(new Entities.Concrete.Car { Id = 6, BrandId = 4, ColorId = 8, DailyPrice = 230000, ModelYear = 2021, Description = "Seat Leon" });
+            Console.WriteLine("The Car is Added. New Car List is Below");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+            Console.WriteLine("-----------Update----------");
+            carManager.Update(new Entities.Concrete.Car { Id = 6, BrandId = 1, ColorId = 8, DailyPrice = 750000, ModelYear = 2021, Description = "BMW 520" });
+            Console.WriteLine("The Car is Updated. New Car List is Below");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,55 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id = 1, BrandId = 1, ColorId = 1, DailyPrice = 550000, ModelYear = 2021, Description = "BMW 320"},
+                new Car{Id = 2, BrandId = 1, ColorId = 3, DailyPrice = 400000, ModelYear = 2021, Description = "BMW 118"},
+                new Car{Id = 3, BrandId = 2, ColorId = 5, DailyPrice = 390000, ModelYear = 2021, Description = "Audi A3"},
+                new Car{Id = 4, BrandId = 3, ColorId = 1, DailyPrice = 150000, ModelYear = 2020, Description = "Fiat Egea"},
+                new Car{Id = 5, BrandId = 4, ColorId = 2, DailyPrice = 3000000, ModelYear = 2021, Description = "Mercedes S"}
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetByBrandId(int brandId)
+        {
+            return _cars.Where(c => c.BrandId == brandId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+        }
+    }
+}
